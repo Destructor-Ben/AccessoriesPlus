420ca17bf798b73cdaba6e3c352199bf083b3b45
Destructor-Ben <destructorben208@gmail.com>
2026-08-03 20:07:49 +1200

    better background colour for object elements

--- Config/Elements/CustomObjectElement.cs
+++ Config/Elements/CustomObjectElement.cs
@@ -1,5 +1,6 @@
 using Terraria.ModLoader.Config.UI;
 using Terraria.ModLoader.UI;
+using Terraria.UI.Chat;
 
 namespace AccessoriesPlus.Config.Elements;
 
@@ -10,9 +11,6 @@ public class CustomObjectElement : ObjectElement
     public override void OnBind()
     {
         base.OnBind();
-
-        // Custom background color
-        backgroundColor = Color.Lerp(UICommon.MainPanelBackground, UICommon.DefaultUIBlue, 0.25f);
 
         // Shrink child elements
         dataList.Width.Pixels *= 2;
@@ -47,6 +45,57 @@ public class CustomObjectElement : ObjectElement
             expandButton.HoverText = Language.GetTextValue("tModLoader.ModConfigExpand");
             expandButton.SetImage(CollapsedTexture);
         }
+    }
+
+    // Copied and modified from ConfigElement
+    protected override void DrawSelf(SpriteBatch spriteBatch)
+    {
+        var dimensions = base.GetDimensions();
+        float settingsWidth = dimensions.Width + 1f;
+        var vector = new Vector2(dimensions.X, dimensions.Y);
+        var baseScale = new Vector2(0.8f);
+        var color = Color.White;
+
+        if (!MemberInfo.CanWrite)
+            color = Color.Gray;
+
+        // Modified: Changed the pan
+        var panelColor = IsMouseHovering ? backgroundColor : backgroundColor.MultiplyRGBA(new Color(180, 180, 180));
+        if (expanded)
+            panelColor = UICommon.MainPanelBackground;
+
+        var position = vector;
+
+        if (Flashing) {
+            float ratio = Utils.Turn01ToCyclic010(((Interface.modConfig.UpdateCount % flashRate) / (float)flashRate)) * 0.5f + 0.5f;
+            panelColor = Color.Lerp(panelColor, Color.White, MathF.Pow(ratio, 2));
+        }
+
+        DrawPanel2(spriteBatch, position, TextureAssets.SettingsPanel.Value, settingsWidth, dimensions.Height, panelColor);
+
+        if (DrawLabel) {
+            position.X += 8f;
+            position.Y += 8f;
+
+            string label = TextDisplayFunction();
+            if (ReloadRequired && ValueChanged) {
+                label += " - [c/FF0000:" + Language.GetTextValue("tModLoader.ModReloadRequired") + "]";
+            }
+
+            ChatManager.DrawColorCodedStringWithShadow(spriteBatch, FontAssets.ItemStack.Value, label, position, color, 0f, Vector2.Zero, baseScale, settingsWidth, 2f);
+        }
+
+        if (!IsMouseHovering || TooltipFunction == null)
+            return;
+
+        string tooltip = TooltipFunction();
+
+        if (ShowReloadRequiredTooltip) {
+            tooltip += string.IsNullOrEmpty(tooltip) ? "" : "\n";
+            tooltip += $"[c/{Color.Orange.Hex3()}:" + Language.GetTextValue("tModLoader.ModReloadRequiredMemberTooltip") + "]";
+        }
+
+        UIModConfig.Tooltip = tooltip;
     }
 }
 
